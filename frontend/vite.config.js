7d0310d15edfc098e6ec3f5a46a3916dcf5c23ad
PrincePatel166 <pp9371881@gmail.com>
2025-04-13 11:41:39 +0530

    update frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server:{
    proxy:{
      "/api":{
-      target: "http://localhost:5000"
+      target: "https://shiksha-backend-vaw7.onrender.com"
      }
    }
   },
