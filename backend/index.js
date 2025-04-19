105ee34a60c7b6dc9c1ecb85a8aaedc145bc93e1
PrincePatel166 <pp9371881@gmail.com>
2025-04-19 12:19:24 +0530

    update backend

--- backend/index.js
+++ backend/index.js
@@ -645,7 +645,7 @@ const app = express();
 
 // CORS Configuration
 app.use(cors({
-  origin: ["http://localhost:5173", "https://shiksha-portal.netlify.app"],
+  origin: ["https://shiksha-portal.netlify.app"],
   credentials: true
 }));
 
